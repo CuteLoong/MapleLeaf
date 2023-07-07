453832aa74a8efdbf5cd003eb1de475375e3e216
CuteLoong <cuteloong@yahoo.com>
2023-07-07 15:30:57 +0800

    添加了Transform相关的内容

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-#include <filesystem>
+#include <vector>
 
 int main(int argc, char** argv)
 {
-    std::filesystem::path path = "/Resources/Shader/tri1.vert";
-    std::cout << std::filesystem::exists(path) << std::endl;
+    std::vector<int> nums = {8, 5, 11, -3, 9, 22, 89, -11, 3, 2};
+
+    int indexMax = nums.size() - 1;
+
+    int ResIndexMax = nums.size() - 1;
+    int ResIndexMin = nums.size() - 1;
+
+    int maxDiff = 0;
+
+    for(int i = nums.size() - 1; i >= 0; i--)
+    {
+        if(nums[i] > nums[indexMax]) indexMax = i;
+
+        int tmpDiff = nums[indexMax] - nums[i];
+        if(tmpDiff > maxDiff) {
+            ResIndexMax = indexMax;
+            ResIndexMin = i;
+            maxDiff = tmpDiff;
+        }
+    }
+
+    std::cout << ResIndexMax << " " << ResIndexMin << std::endl;
 
     return 0;
 }
